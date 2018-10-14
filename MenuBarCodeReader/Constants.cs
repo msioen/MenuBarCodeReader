2a22a9db8e4e4530cdfa4e485accd9bf291828f8
Michiel Sioen <michiel_sioen@hotmail.com>
2018-10-14 19:46:44 +0200

    #1 - send notifications on success / failure - clicking the notification copies result again to clipboard

--- MenuBarCodeReader/Constants.cs
+++ MenuBarCodeReader/Constants.cs
@@ -12,5 +12,8 @@ namespace MenuBarCodeReader
         public static readonly NSString KEY_HOTKEY_SCAN_SELECT = new NSString("values.hotKeyScanSelect");
         public static readonly NSString KEY_HOTKEY_ESC = new NSString("values.hotKeyEscape");
         public static readonly NSString KEY_HOTKEY_SCAN_CLICK = new NSString("values.hotKeyScanClick");
+
+        public const string SETTING_OUTPUT_CLIPBOARD = "SETTING_OUTPUT_CLIPBOARD";
+        public const string SETTING_OUTPUT_NOTIFICATION = "SETTING_OUTPUT_NOTIFICATION";
     }
 }
---
+++ MenuBarCodeReader/Utils/Settings.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Foundation;
+
+namespace MenuBarCodeReader
+{
+    public class Settings
+    {
+        #region Public
+
+        public bool ShouldOutputScanResultsToClipboard
+        {
+            get { return GetOrDefault(Constants.SETTING_OUTPUT_CLIPBOARD, true); }
+            set { NSUserDefaults.StandardUserDefaults.SetBool(value, Constants.SETTING_OUTPUT_CLIPBOARD); }
+        }
+
+        public bool ShouldOutputScanResultsToNotificationCenter
+        {
+            get { return GetOrDefault(Constants.SETTING_OUTPUT_NOTIFICATION, true); }
+            set { NSUserDefaults.StandardUserDefaults.SetBool(value, Constants.SETTING_OUTPUT_NOTIFICATION); }
+        }
+
+        #endregion
+
+        #region Private
+
+        bool GetOrDefault(string key, bool defaultValue)
+        {
+            if (NSUserDefaults.StandardUserDefaults[key] == null)
+                return defaultValue;
+
+            return NSUserDefaults.StandardUserDefaults.BoolForKey(key);
+        }
+
+        #endregion
+    }
+}
